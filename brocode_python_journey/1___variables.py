603e0056ad4f8d273db1a89048373d75fd3533dd
BOUDKHOUM-ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-09-28 13:08:33 +0100

    update

--- brocode_python_journey/1___variables.py
+++ brocode_python_journey/1___variables.py
@@ -1,14 +1,23 @@
 
 
 #sring
+from operator import is_
+
+
 first_name= "Bro code"
 print(first_name)
 print(f"Hello {first_name}") #fstring
 
-
 food=("pizza")
 print(f"you like {food}")
 
-
+#integer
 age =23
 print(f"you have {age} years old")
+
+#bool
+is_student=True
+if is_student:
+    print("you are a student")
+else :
+     print("you are not a student")
