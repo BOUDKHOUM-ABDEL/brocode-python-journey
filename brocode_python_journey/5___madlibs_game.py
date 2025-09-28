4430f18dd2f1bf3d1602a42c95ec96387ab436c8
BOUDKHOUM-ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-09-28 13:51:38 +0100

    madlibs game

---
+++ brocode_python_journey/5___madlibs_game.py
@@ -0,0 +1,12 @@
+#madlibs game where you create
+#  a story by filling in blanks with a ramdon words 
+
+adjective1=input("Enter an adjective (Description)   : ")
+nom1=input("Enter a name (person , place , things...)  : ")
+adjective2=input("Enter an adjective (Description)   : ")
+verb1=input("Enter a verb ending by 'ing' :" )
+
+
+print(f"today I went to {adjective1} zoo ")
+print(f"in an exhibit I saw a {nom1} ")
+print(f"{nom1} was {adjective2} and {verb1}")
