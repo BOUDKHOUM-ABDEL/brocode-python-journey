394f5d73c15f1648ab1cec9d41db5a3b83e54e7f
BOUDKHOUM-ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-09-28 13:01:04 +0100

    update

---
+++ brocode_python_journey/1___variables.py
@@ -0,0 +1,14 @@
+
+
+#sring
+first_name= "Bro code"
+print(first_name)
+print(f"Hello {first_name}") #fstring
+
+
+food=("pizza")
+print(f"you like {food}")
+
+
+age =23
+print(f"you have {age} years old")
