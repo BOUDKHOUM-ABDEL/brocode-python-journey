457c9f088868483e05b5e917e0442a5cc7f67eba
BOUDKHOUM-ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-09-28 13:24:12 +0100

    typecasting part

---
+++ brocode_python_journey/3___input().py
@@ -0,0 +1,15 @@
+
+
+
+#input ("what's your name?")  
+name =input ("what's your name?  : ")
+
+print(f"Hello {name}")
+
+#__________________________user input___________________________
+lengh=float(input("Enter the lengh  : "))
+width=float(input("Enter the Width : "))
+area=lengh*width
+
+print (f"the area is {area}cm2")
+
