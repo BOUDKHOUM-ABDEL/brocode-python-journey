062b1a6b0cfae0d953203396e4fafbedd9084f4c
BOUDKHOUM-ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-09-28 17:53:57 +0100

    python calculator program

---
+++ brocode_python_journey/8__toDo_pythonCalculator.py
@@ -0,0 +1,26 @@
+
+from tkinter import ROUND
+
+
+operator=input("Enter operator (+, -, *, /): ")
+
+num1=float(input("Enter first number: "))
+num2=float(input("Enter second number: "))
+
+if operator=="+":
+    result=num1+num2
+    print(f"{num1} + {num2} = {round(result,2)}")
+elif operator=="-":
+    result=num1-num2
+    print(f"{num1} - {num2} = {round(result,2)}")
+elif operator=="*":
+    result=num1*num2
+    print(f"{num1} * {num2} = {round(result,2)}")
+elif operator=="/":
+    if num2!=0:
+        result=num1/num2
+        print(f"{num1} / {num2} = {round(result,2)}")
+    else:
+        print("Error: Division by zero is not allowed.")
+else:
+    print("Invalid operator")
