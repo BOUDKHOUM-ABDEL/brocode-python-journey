b472170569f9e993fefcf8e074a39e3c7955e794
BOUDKHOUM-ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-09-28 23:32:42 +0100

    string methods

---
+++ part_2/1___string methods.py
@@ -0,0 +1,18 @@
+
+
+name=input("Enter your name: ")
+phone_number=input("Enter your phone number: ")
+
+result=len(name) # length of string 
+result=name.upper() # convert to uppercase
+result=name.lower() # convert to lowercase
+result=name.title() # convert to title case (first letter of each word capitalized)
+result=name.count("a") # count occurrences of a specific character
+result=name.isalpha() # check if all characters are alphabetic
+result=name.capitalize() # capitalize the first letter of the string
+result=name.replace("a","o") # replace occurrences of a specific character with another character
+result=phone_number.isdigit() # check if all characters are digits
+result=phone_number.count("1") # count occurrences of a specific digit)
+
+
+print(result)
