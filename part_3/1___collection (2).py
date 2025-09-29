a82ffb90a32e0b0604ab9236ad99334948c063f6
BOUDKHOUM-ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-09-29 15:55:11 +0100

    update

---
+++ part_3/1___collection (2).py
@@ -0,0 +1,18 @@
+
+
+#collections = single 'variable' used to store multiple values.
+
+#list = [] collection which is ordered and changeable.  Deplicate ok.
+#Set = {} collection which is unordered and immutibale. can add/remove. No deplicate.  
+#Tuple = () collection which is ordered and unchangeable.  Deplicate ok. faster than list.
+
+fruits = {"apple", "banana", "coconut"}
+print(fruits) # {'banana', 'coconut', 'apple'}
+#print(fruits[2]) # coconut (error, set does not support indexing
+
+fruits.append("orange") # add item to end of list
+fruits.insert("kiwi") # add item
+fruits.remove("banana") # remove item by value
+print(fruits) # {'kiwi', 'coconut', 'apple', 'orange'}
+fruits.pop() # remove last item
+print(fruits) # {'kiwi', 'coconut', 'apple'}
---
+++ part_3/1___list.py
@@ -0,0 +1,25 @@
+
+
+#collections = single 'variable' used to store multiple values.
+
+#list = [] collection which is ordered and changeable.  Deplicate ok.
+#Set = {} collection which is unordered and immutibale. can add/remove. No deplicate.  
+#Tuple = () collection which is ordered and unchangeable.  Deplicate ok. faster than list.
+
+fruits = ["apple", "banana", "coconut"]
+print(fruits) # ['apple', 'banana', 'coconut']
+print(fruits[2]) # coconut
+print(fruits[-1]) # coconut (last item)
+print(fruits[0:2]) # ['apple', 'banana'] (first 2 items)
+fruits[0] = "pear" # change item
+for fruit in fruits: 
+    print(fruit , end=" ") # pear banana coconut (print all items in one line)
+
+fruits.append("orange") # add item to end of list
+fruits.insert(1, "kiwi") # add item at index 1
+fruits.remove("banana") # remove item by value
+print(fruits) # ['pear', 'kiwi', 'coconut', 'orange']
+fruits.sort() # sort list alphabetically
+print(fruits) # ['coconut', 'kiwi', 'orange', 'pear']
+fruits.pop() # remove last item
+print(fruit.index("kiwi")) # get index of item
