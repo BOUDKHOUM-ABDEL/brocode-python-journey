beadf9437b235ebc52f1e38452eb8f52144fc9a8
BOUDKHOUM-ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-09-29 15:35:11 +0100

    update

---
+++ part_2/6___for_loop.py
@@ -0,0 +1,23 @@
+
+
+for x in range(5):
+    print(x)  # prints 0, 1, 2, 3, 4 on separate lines
+
+for x in range(5):
+    print(x ,end=" ")  # prints 0, 1, 2, 3, 4 on the same lines
+
+for x in range(2, 5):
+    print(x)  # prints 2, 3, 4 on separate lines
+
+for x in range(2, 10, 3):
+    print(x)  # prints 2, 5, 8 on separate lines
+
+for x in range(1,5):
+    print(x * '*')  # prints a triangle of asterisks
+    
+for x in range(1,5):
+    for y in  range(1, x + 1):
+        print(x*'*')
+        break
+
+
