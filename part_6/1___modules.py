c780d2f14b7fe50ccf3984aa20a1780902d25687
BOUDKHOUM-ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-10-02 14:08:54 +0100

    update

---
+++ part_6/1___modules.py
@@ -0,0 +1,19 @@
+#module = a file containing code you want to include in your program 
+#         use import to include a module
+
+import math
+from unittest import result 
+print(math.pi)
+
+import math as m 
+print(m.e)
+
+from math import pi
+print(pi)
+
+import module2
+
+print(module2.subtract(10,5))
+result = module2.multiply(4,7)
+print(result)
+
