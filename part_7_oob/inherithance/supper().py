e8cd660d8bdc995b79647a9cefdc0b996745d5a2
BOUDKHOUM-ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-10-06 19:21:30 +0100

    inheritence

--- part_7_oob/inherithance/supper().py
+++ part_7_oob/inherithance/supper().py
@@ -41,6 +41,10 @@ class Triangle(Shape):
         self.radius = width
         self.hight = hight
 
+    def Describe(self):
+        super().Describe()
+        print(f"It's a Triangle with Area of {0.5*self.radius*self.hight} cm2")
+
 
 circle1= Circle("red", True, 5)
 print(circle1.color) # 'red'
@@ -50,12 +54,15 @@ square1= Square("blue", False, 4)
 print(square1.color) # 'blue'
 print(square1.is_filled) # False
 
-Triangle1=Triangle(color="blue", is_filled=True, width=6 , hight=3)
+Triangle1=Triangle(color="blue", is_filled=True, width=6 , hight=3)  
 
-print(Triangle1.color)
-Triangle1.color="Yellow"
-print(Triangle1.color)
+print(Triangle1.color) # 'blue'
+Triangle1.color="Yellow" # change the color attribute value
+print(Triangle1.color) # 'Yellow'
  
 
 
-circle1.Describe() #output: 
+circle1.Describe() # output: It's red and filled. It's a circle with Area of 78.5 cm2   
+square1.Describe() # output: It's blue and not filled. It's a Square with Area of 16 cm2
+Triangle1.Describe() # output: It's yellow and filled.
+Triangle1.Describe() # output: It's a Triangle with Area of 9.0 cm2
