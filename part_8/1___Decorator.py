20af987d7ec06c3d2185c44a24b0d6edca0007a2
BOUDKHOUM-ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-10-09 13:51:35 +0100

    add

--- part_8/1___Decorator.py
+++ part_8/1___Decorator.py
@@ -4,39 +4,28 @@
 # Decorators are commonly used for logging, access control, timing, and more.
 
 def add_spinkles(func):
-    def wrapper():
+    def wrapper(*args,**kwargs):
         print("you add spinkles")
-        func()
+        func(*args,**kwargs)
+    return wrapper
+
+def add_frdge(func):
+    def wrapper(*args,**kwargs):
+        print("you add fudge ")
+        func(*args,**kwargs)
     return wrapper
 
 @add_spinkles
-def make_cake():
-    print("Cake is ready!")
+@add_frdge 
+def get_ice_cream(flavor):
+    print(f"Here is your {flavor} !")
 
 
 
-make_cake()
-# Output:
-# you add spinkles
-# Cake is ready!
-
-# Example: Decorator with arguments
-def repeat(n):
-    def decorator(func):
-        def wrapper(*args, **kwargs):
-            for _ in range(n):
-                func(*args, **kwargs)
-        return wrapper
-    return decorator
-
-@repeat(3)
-def say_hello():
-    print("Hello!")
+get_ice_cream("vanilla")
 
 
+#you add spinkles
+#you add fudge
+#Here is your ice cream !
 
-say_hello()
-# Output:
-# Hello!
-# Hello!
-# Hello!
