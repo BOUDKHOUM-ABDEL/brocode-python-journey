a832af588fdf41ddb07a983275b3be911f801d4f
BOUDKHOUM-ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-10-09 18:59:50 +0100

    update

--- part_7_oob/8___property.py
+++ part_7_oob/8___property.py
@@ -1,3 +1,9 @@
 
 
-#@property = 
+#@property = Decorator user to define a methode as property(it can be accessed like attribute)
+#            Benefit : Add additional logic when read, wrhite , or Delete attributes
+#            Gives you getter , setter , and deleter method
+
+class Rectangle:
+
+
