a381b817d3bc24b8792e60c1371657403205a5da
BOUDKHOUM-ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-10-10 15:05:24 +0100

    add

---
+++ Numpy/1__introduction.py
@@ -0,0 +1,13 @@
+
+import numpy as np
+
+my_list =[1,2,3,4]
+print(my_list)  #[1,2,3,4]
+
+my_list =[1,2,3,4]*2 #[1, 2, 3, 4, 1, 2, 3, 4]
+print(my_list)
+
+array=np.array([1,2,3,4])
+print(array)  #[1 2 3 4]
+array = array * 2
+print(array)  #[2 4 6 8]
