b9ed619a5632686e0c7517310fe4f36fada57cde
BOUDKHOUM-ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-10-11 11:50:01 +0100

    create file

--- Numpy/5__broadcasting.py
+++ Numpy/5__broadcasting.py
@@ -22,7 +22,7 @@ print(array1 * array2)
 array1 = np.array([[1,2,3,4,5,6,7,8,9,10]])
 array2 = np.array([[1],[2],[3],[4],[5],[6],[7],[8],[9],[10]])
 
-print(array1.shape)  #(1, 4)
-print(array2.shape)  #(4, 1)
+print(array1.shape)  #(1, 10)
+print(array2.shape)  #(10, 1)
 
 print(array1 * array2)
